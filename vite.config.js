90cb1d06f93b896a18007644a30f01c6d0c2fea5
bohdansas-alt <bohdan.sas@elbax.com>
2026-06-27 01:05:32 +0300

    Fix production CSS compatibility

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  build: {
+    cssTarget: ["chrome90", "safari14"],
+    cssMinify: false,
+  },
   plugins: [react()],
 });
